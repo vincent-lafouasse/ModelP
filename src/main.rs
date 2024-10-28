e6775e76b3548896507003c2982b800947200307
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-28 23:03:28 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -8,38 +8,62 @@ use cpal::Data;
 
 const WAVETABLE_RESOLUTION: usize = 256;
 
-fn main() {
-    let sine_wavetable: Vec<f32> = (0..WAVETABLE_RESOLUTION)
-        .map(|i| 2.0 * PI * (i as f32) / (WAVETABLE_RESOLUTION as f32))
+fn build_sine_wavetable(resolution: usize) -> Arc<[f32]> {
+    (0..resolution)
+        .map(|i| 2.0 * PI * (i as f32) / (resolution as f32))
         .map(|phase| phase.sin())
-        .collect();
-    let sine_wavetable: Arc<[f32]> = sine_wavetable.into();
+        .collect()
+}
 
-    let host = cpal::default_host();
-    let device = host
-        .default_output_device()
-        .expect("no output device available");
+struct Synth {
+    wavetable: Arc<[f32]>,
+    phase: f32,
+}
 
-    let mut supported_configs_range = device
-        .supported_output_configs()
-        .expect("error while querying configs");
-    let stream_config = supported_configs_range
-        .next()
-        .expect("no supported config?!")
-        .with_max_sample_rate();
-    let stream = device
-        .build_output_stream(
-            &stream_config.config(),
-            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-                // react to stream events and read or write stream data here.
-            },
-            move |err| {
-                // react to errors here.
-            },
-            None, // None=blocking, Some(Duration)=timeout
-        )
-        .expect("failed to open output stream");
+impl Synth {
+    fn new() -> Self {
+        let wavetable = build_sine_wavetable(WAVETABLE_RESOLUTION);
+        let phase = 0.0;
 
-    stream.play();
-    std::thread::sleep(std::time::Duration::from_secs(1));
+        Self { wavetable, phase }
+    }
+
+    fn open_stream(&self) {
+        let host = cpal::default_host();
+        let device = host
+            .default_output_device()
+            .expect("no output device available");
+
+        let mut supported_configs_range = device
+            .supported_output_configs()
+            .expect("error while querying configs");
+        let stream_config = supported_configs_range
+            .next()
+            .expect("no supported config?!")
+            .with_max_sample_rate();
+        let stream = device
+            .build_output_stream(
+                &stream_config.config(),
+                move |data: &mut [f32], info: &cpal::OutputCallbackInfo| {
+                    callback(data, info);
+                },
+                move |err| {
+                    // react to errors here.
+                },
+                None,
+            )
+            .expect("failed to open output stream");
+        stream.play();
+    }
+}
+
+fn callback(data: &mut [f32], info: &cpal::OutputCallbackInfo) {
+    for sample in data {
+        *sample = 0.0;
+    }
+}
+
+fn main() {
+    let synth = Synth::new();
+    synth.open_stream();
 }
