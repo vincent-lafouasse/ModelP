fcf638c751f457cfe8452f23dda64917cc4fdcb6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-28 22:42:15 +0100

    construct sine wavetable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+#![allow(unused)]
+
+use std::f32::consts::PI;
+use std::sync::Arc;
+
+const WAVETABLE_RESOLUTION: usize = 256;
+
 fn main() {
-    println!("Hello, world!");
+    let sine_wavetable: Vec<f32> = (0..WAVETABLE_RESOLUTION)
+        .map(|i| 2.0 * PI * (i as f32) / (WAVETABLE_RESOLUTION as f32))
+        .map(|phase| phase.sin())
+        .collect();
+    let sine_wavetable: Arc<[f32]> = sine_wavetable.into();
+
+    dbg!(sine_wavetable);
 }
