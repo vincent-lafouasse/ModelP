43c1be8e4ee6fb6d397bcba1bc1100af70bd3395
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-28 22:50:09 +0100

    open an output stream (probably)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@
 use std::f32::consts::PI;
 use std::sync::Arc;
 
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use cpal::Data;
+
 const WAVETABLE_RESOLUTION: usize = 256;
 
 fn main() {
@@ -12,5 +15,31 @@ fn main() {
         .collect();
     let sine_wavetable: Arc<[f32]> = sine_wavetable.into();
 
-    dbg!(sine_wavetable);
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+
+    let mut supported_configs_range = device
+        .supported_output_configs()
+        .expect("error while querying configs");
+    let stream_config = supported_configs_range
+        .next()
+        .expect("no supported config?!")
+        .with_max_sample_rate();
+    let stream = device
+        .build_output_stream(
+            &stream_config.config(),
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                // react to stream events and read or write stream data here.
+            },
+            move |err| {
+                // react to errors here.
+            },
+            None, // None=blocking, Some(Duration)=timeout
+        )
+        .expect("failed to open output stream");
+
+    stream.play();
+    std::thread::sleep(std::time::Duration::from_secs(1));
 }
