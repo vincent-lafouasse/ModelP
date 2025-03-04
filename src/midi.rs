689c84fcd35d71d33b8a24455aa30e04a80d7d67
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-04 20:45:00 +0100

    init MidiNote struct

--- src/midi.rs
+++ src/midi.rs
@@ -1,18 +1,36 @@
 use std::time::Instant;
 
+pub struct MidiNote {
+    offset: u8
+}
+
+impl MidiNote {
+    pub fn new(offset: u8) -> Self {
+        Self {offset}
+    }
+
+    // 12TET
+    pub fn frequency(&self) -> f32 {
+        let offset_from_a4: u8 = self.offset - 60;
+
+        440.0 * 2.0_f32.powf(offset_from_a4 as f32 / 12.0)
+    }
+}
+
 pub enum MidiEventKind {
     NoteOn,
     NoteOff,
 }
 
 pub struct MidiEvent {
-    pub note: u8,
+    pub note: MidiNote,
     pub kind: MidiEventKind,
     pub timestamp: Instant,
 }
 
 impl MidiEvent {
     pub fn note_on(note: u8) -> Self {
+        let note = MidiNote::new(note);
         let kind = MidiEventKind::NoteOn;
         let timestamp = Instant::now();
         Self {
@@ -23,6 +41,7 @@ impl MidiEvent {
     }
 
     pub fn note_off(note: u8) -> Self {
+        let note = MidiNote::new(note);
         let kind = MidiEventKind::NoteOff;
         let timestamp = Instant::now();
         Self {
@@ -30,12 +49,5 @@ impl MidiEvent {
             kind,
             timestamp,
         }
-    }
-
-    // 12TET
-    pub fn frequency(&self) -> f32 {
-        let offset_from_a4: u8 = self.note - 60;
-
-        440.0 * 2.0_f32.powf(offset_from_a4 as f32 / 12.0)
     }
 }
