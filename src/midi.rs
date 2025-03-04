237ebed6e2aec8d2e869598d03a9964a98ba886f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-04 21:57:52 +0100

    refactor

--- src/midi.rs
+++ src/midi.rs
@@ -18,39 +18,36 @@ impl MidiNote {
     }
 
     #[allow(dead_code)]
+    pub fn offset_up(&self, n: u8) -> Self {
+        let note: u8 = self.note.saturating_add(n);
+        let note: u8 = if note > 127 { 127 } else { note };
+        Self { note }
+    }
+
+    #[allow(dead_code)]
+    pub fn offset_down(&self, n: u8) -> Self {
+        let note: u8 = self.note.saturating_sub(n);
+        Self { note }
+    }
+
+    #[allow(dead_code)]
+    pub fn octave_up(&self, n: u8) -> Self {
+        self.offset_up(12 * n)
+    }
+
+    #[allow(dead_code)]
+    pub fn octave_down(&self, n: u8) -> Self {
+        self.offset_down(12 * n)
+    }
+
+    #[allow(dead_code)]
     pub fn c0() -> Self {
-        let note = 12;
-        Self { note }
+        Self { note: 12 }
     }
 
     #[allow(dead_code)]
-    pub fn c1() -> Self {
-        let note = Self::c0().note + 12;
-        Self { note }
-    }
-
-    #[allow(dead_code)]
-    pub fn c2() -> Self {
-        let note = Self::c0().note + 2 * 12;
-        Self { note }
-    }
-
-    #[allow(dead_code)]
-    pub fn c3() -> Self {
-        let note = Self::c0().note + 3 * 12;
-        Self { note }
-    }
-
-    #[allow(dead_code)]
-    pub fn c4() -> Self {
-        let note = Self::c0().note + 4 * 12;
-        Self { note }
-    }
-
-    #[allow(dead_code)]
-    pub fn c5() -> Self {
-        let offset = Self::c0().note + 5 * 12;
-        Self { note: offset }
+    pub fn c(octave: u8) -> Self {
+        Self::c0().octave_up(octave)
     }
 }
 
