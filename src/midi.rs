aed562804393c341241d5e0954351db736db010a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-04 20:55:21 +0100

    introduce convenience constructors MidiNote::c[012345]

--- src/midi.rs
+++ src/midi.rs
@@ -1,12 +1,13 @@
+use sdl2::sys::wchar_t;
 use std::time::Instant;
 
 pub struct MidiNote {
-    offset: u8
+    offset: u8,
 }
 
 impl MidiNote {
     pub fn new(offset: u8) -> Self {
-        Self {offset}
+        Self { offset }
     }
 
     // 12TET
@@ -14,6 +15,42 @@ impl MidiNote {
         let offset_from_a4: u8 = self.offset - 60;
 
         440.0 * 2.0_f32.powf(offset_from_a4 as f32 / 12.0)
+    }
+
+    #[allow(dead_code)]
+    pub fn c0() -> Self {
+        let offset = 12;
+        Self { offset }
+    }
+
+    #[allow(dead_code)]
+    pub fn c1() -> Self {
+        let offset = Self::c0().offset + 12;
+        Self { offset }
+    }
+
+    #[allow(dead_code)]
+    pub fn c2() -> Self {
+        let offset = Self::c0().offset + 2 * 12;
+        Self { offset }
+    }
+
+    #[allow(dead_code)]
+    pub fn c3() -> Self {
+        let offset = Self::c0().offset + 3 * 12;
+        Self { offset }
+    }
+
+    #[allow(dead_code)]
+    pub fn c4() -> Self {
+        let offset = Self::c0().offset + 4 * 12;
+        Self { offset }
+    }
+
+    #[allow(dead_code)]
+    pub fn c5() -> Self {
+        let offset = Self::c0().offset + 5 * 12;
+        Self { offset }
     }
 }
 
