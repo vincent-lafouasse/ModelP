ff058925f8e7438e5948b4a3d6f0fd845a19179b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-04 22:12:58 +0100

    fix typo

--- src/midi.rs
+++ src/midi.rs
@@ -1,4 +1,3 @@
-use sdl2::sys::wchar_t;
 use std::time::Instant;
 
 pub struct MidiNote {
@@ -12,7 +11,7 @@ impl MidiNote {
 
     // 12TET
     pub fn frequency(&self) -> f32 {
-        let offset_from_a4: i8 = 69 - self.note as i8;
+        let offset_from_a4: i8 = self.note as i8 - 69;
 
         440.0 * 2.0_f32.powf(offset_from_a4 as f32 / 12.0)
     }
