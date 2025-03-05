8eb23a2c3fc5423e3acf2d8a2f073e3c37b29ce6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-05 19:20:29 +0100

    sketch vca

--- src/envelope.rs
+++ src/envelope.rs
@@ -1,3 +1,5 @@
+use crate::midi;
+use cpal::OutputStreamTimestamp;
 use std::time::Duration;
 
 pub struct Envelope {
@@ -5,8 +7,33 @@ pub struct Envelope {
     release: Duration,
 }
 
+type StreamEventKind = crate::midi::MidiEventKind;
+pub struct StreamEvent {
+    kind: StreamEventKind,
+    timestamp: OutputStreamTimestamp,
+}
+
 impl Envelope {
     pub fn new(attack: Duration, release: Duration) -> Self {
         Self { attack, release }
+    }
+
+    pub fn volume(&self, now: OutputStreamTimestamp, last_event: Option<StreamEvent>) -> f32 {
+        if let Some(StreamEvent { kind, timestamp }) = last_event {
+            let full_duration = match kind {
+                StreamEventKind::NoteOn => self.attack,
+                StreamEventKind::NoteOff => self.release,
+            };
+            let advancement: Duration = now.playback.duration_since(&timestamp.playback).unwrap();
+            let advancement = advancement.as_nanos() as f32 / full_duration.as_nanos() as f32;
+            let advancement = advancement.clamp(0.0, 1.0);
+
+            match kind {
+                StreamEventKind::NoteOn => advancement,
+                StreamEventKind::NoteOff => 1.0 - advancement,
+            }
+        } else {
+            0.0
+        }
     }
 }
