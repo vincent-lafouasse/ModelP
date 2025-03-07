97a375b9f6bd792c1f35f443421cf442170718b3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-03-07 17:11:52 +0100

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,7 @@
 #![allow(unused_variables)]
 
 use std::collections::HashSet;
-use std::time::{Duration, Instant};
 
-extern crate eframe;
-
-use egui;
 use egui::Key;
 
 mod event;
@@ -19,9 +15,6 @@ mod wavetable;
 use crate::event::Event;
 use crate::midi::MidiNote;
 use crate::synth::Synth;
-
-const TARGET_REFRESH_RATE: f32 = 200.0;
-const FRAME_LEN: Duration = Duration::from_nanos((1_000_000_000f32 / TARGET_REFRESH_RATE) as u64);
 
 struct App {
     synth: Synth,
@@ -141,108 +134,3 @@ fn keymap(keycode: &Key, root: MidiNote) -> Option<MidiNote> {
         _ => None,
     }
 }
-
-/*
-
-pub fn main() -> Result<(), String> {
-    let mut synth = Synth::new();
-    let mut pressed_keys: HashSet<Keycode> = HashSet::new();
-    let mut root_note = MidiNote::c(2);
-
-    let rendering_ctx = RenderingContext::new();
-    let mut canvas = rendering_ctx.make_canvas();
-    canvas.set_draw_color(Color::RGB(161, 88, 255)); // purple background
-
-    let mut event_pump = rendering_ctx.sdl_context.event_pump()?;
-    'running: loop {
-        let frame_start = Instant::now();
-        for event in event_pump.poll_iter() {
-            match event {
-                SdlEvent::Quit { .. }
-                | SdlEvent::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                SdlEvent::KeyDown {
-                    keycode: Some(Keycode::Z),
-                    ..
-                } => root_note = root_note.octave_down(1),
-                SdlEvent::KeyDown {
-                    keycode: Some(Keycode::X),
-                    ..
-                } => root_note = root_note.octave_up(1),
-                SdlEvent::KeyUp {
-                    keycode: Some(keycode),
-                    ..
-                }
-                | SdlEvent::KeyDown {
-                    keycode: Some(keycode),
-                    ..
-                } => match event {
-                    SdlEvent::KeyDown { .. } => {
-                        if let Some(note) = keymap(keycode, root_note) {
-                            if !pressed_keys.contains(&keycode) {
-                                synth.send_midi_event(Event::NoteOn(note));
-                                pressed_keys.insert(keycode);
-                            }
-                        }
-                    }
-                    SdlEvent::KeyUp { .. } => {
-                        if let Some(note) = keymap(keycode, root_note) {
-                            if pressed_keys.contains(&keycode) {
-                                synth.send_midi_event(Event::NoteOff(note));
-                                pressed_keys.remove(&keycode);
-                            }
-                        }
-                    }
-                    _ => unreachable!(),
-                },
-                _ => {}
-            }
-        }
-
-        canvas.clear();
-        canvas.present();
-
-        std::thread::sleep(FRAME_LEN.saturating_sub(frame_start.elapsed()));
-    }
-
-    Ok(())
-}
-
-
-struct RenderingContext {
-    sdl_context: sdl2::Sdl,
-    video_subsystem: sdl2::VideoSubsystem,
-}
-
-impl RenderingContext {
-    fn new() -> Self {
-        let sdl_context = sdl2::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
-
-        Self {
-            sdl_context,
-            video_subsystem,
-        }
-    }
-
-    fn make_canvas(&self) -> sdl2::render::WindowCanvas {
-        let window = self
-            .video_subsystem
-            .window("decapode", 800, 600)
-            .position_centered()
-            .opengl()
-            .build()
-            .map_err(|e| e.to_string())
-            .unwrap();
-        window
-            .into_canvas()
-            .build()
-            .map_err(|e| e.to_string())
-            .unwrap()
-    }
-}
-
-
- */
